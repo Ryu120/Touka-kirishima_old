18b6da5973239851a4e3a13ef4126f210d0c3ef7
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-10-15 09:39:21 +0530

    Update shippering.py

--- GabiBraunRobot/modules/shippering.py
+++ GabiBraunRobot/modules/shippering.py
@@ -21,9 +21,9 @@ SOFTWARE.
 
 import random
 
-from Cutiepii_Robot import pgram
-from Cutiepii_Robot.utils.errors import capture_err
-from Cutiepii_Robot.modules.mongo.couples_mongo import get_couple, save_couple
+from Maki_ryu_bot import pgram
+from Maki_ryu_bot.utils.errors import capture_err
+from Maki_ryu_bot.modules.mongo.couples_mongo import get_couple, save_couple
 from pyrogram import filters
 from datetime import datetime
 
