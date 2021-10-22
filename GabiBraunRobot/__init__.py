f112fc5ca316783c3bb715ad85c74bf561f12e48
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-10-22 10:39:55 +0530

    Update __init__.py

--- GabiBraunRobot/__init__.py
+++ GabiBraunRobot/__init__.py
@@ -199,7 +199,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Natsuki.modules.helper_funcs.handlers import (
+from GabiBraunRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
